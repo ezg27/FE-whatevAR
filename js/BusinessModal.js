5688751786f90ed978a06e30e716759cb8bc8e4b
ezg27 <georgehams27@gmail.com>
2018-09-28 09:53:37 +0100

    refactor styling

--- js/BusinessModal.js
+++ js/BusinessModal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import hygiene_rating from './res/foodHygiene/index.js'
+import hygiene_rating from './res/foodHygiene/index.js';
 import {
   Text,
   TouchableHighlight,
@@ -9,53 +9,113 @@ import {
   Image,
   ImageBackground,
   Linking
-}
-  from 'react-native'
+} from 'react-native';
 
 export default class BusinessModal extends Component {
-
   state = {
     business: {},
     error: false
-  }
+  };
 
   render() {
-    const lookupDays = ['Mon:', 'Tue:', 'Wed:', 'Thu:', 'Fri:', 'Sat:', 'Sun:']
+    const lookupDays = ['Mon:', 'Tue:', 'Wed:', 'Thu:', 'Fri:', 'Sat:', 'Sun:'];
     if (Object.keys(this.state.business).length !== 0) {
       return (
-        <View style={localStyles.inner} >
-          <View style={{ height: this.state.business.name.length < 20 ? '23%' : '23%', width: '100%' }}>
-            <ImageBackground source={require('./res/Mojito.jpg')} style={{ width: '100%', height: '100%', bottom: 10 }}>
-              <Text style={localStyles.titleText}>{this.state.business.name}</Text>
-              {this.state.business.address ? <Text style={{ fontSize: 20, textAlign: 'center', color: 'white' }}>{this.state.business.address}</Text> : null}
-              {this.state.business.postcode ? <Text style={{ fontSize: 20, textAlign: 'center', color: 'white' }}>{this.state.business.postcode}</Text> : null}
-              {this.state.business.phone ? <Text style={{ fontSize: 20, textAlign: 'center', color: 'white' }}>{this.state.business.phone}</Text> : null}
+        <View style={localStyles.inner}>
+          <View
+            style={{
+              height: '23%',
+              width: '100%'
+            }}
+          >
+            <ImageBackground
+              source={require('./res/Mojito.jpg')}
+              style={{ width: '100%', height: '100%', bottom: 10 }}
+            >
+              <Text style={localStyles.titleText}>
+                {this.state.business.name}
+              </Text>
+              {this.state.business.address ? (
+                <Text
+                  style={localStyles.businessInfo}
+                >
+                  {this.state.business.address}
+                </Text>
+              ) : null}
+              {this.state.business.postcode ? (
+                <Text
+                  style={localStyles.businessInfo}
+                >
+                  {this.state.business.postcode}
+                </Text>
+              ) : null}
+              {this.state.business.phone ? (
+                <Text
+                  style={localStyles.businessInfo}
+                >
+                  {this.state.business.phone}
+                </Text>
+              ) : null}
             </ImageBackground>
           </View>
           <View style={localStyles.images}>
             {this.state.business.photos.map(photo => {
-              return <Image style={localStyles.singleImage} source={{ uri: photo }} />
+              return (
+                <Image
+                  style={localStyles.singleImage}
+                  source={{ uri: photo }}
+                />
+              );
             })}
           </View>
-          <TouchableHighlight style={localStyles.yelpButton}
-            underlayColor={'#68a0ff'} onPress={this._yelpLink}>
-            <Image source={require('./res/Yelp_burst_positive_RGB_2.png')} resizeMode='stretch' style={{ flex: 1, height: 30, width: 100, margin: 'auto' }} />
+          <TouchableHighlight
+            style={localStyles.yelpButton}
+            underlayColor={'#68a0ff'}
+            onPress={this._yelpLink}
+          >
+            <Image
+              source={require('./res/Yelp_burst_positive_RGB_2.png')}
+              resizeMode="stretch"
+              style={{ flex: 1, height: 30, width: 100, margin: 'auto' }}
+            />
           </TouchableHighlight>
-          {this.state.business.isOpen === null ? null : this.state.business.isOpen ? <Text style={localStyles.open}>Open now</Text> : <Text style={localStyles.closed}>Closed now</Text>}
+          {this.state.business.isOpen === null ? null : this.state.business
+            .isOpen ? (
+            <Text style={localStyles.open}>Open now</Text>
+          ) : (
+            <Text style={localStyles.closed}>Closed now</Text>
+          )}
           <View style={localStyles.openingTimes}>
             {this.state.business.hours.map((hours, i) => {
-              return <View style={localStyles.day}><Text style={localStyles.text}>{lookupDays[i]} </Text><Text style={localStyles.hours}>{hours}</Text></View>
+              return (
+                <View style={localStyles.day}>
+                  <Text style={localStyles.text}>{lookupDays[i]} </Text>
+                  <Text style={localStyles.hours}>{hours}</Text>
+                </View>
+              );
             })}
           </View>
-          {this.state.business.foodRating ? <Image style={{ width: 115, height: 58, margin: 5 }} source={hygiene_rating[this.state.business.foodRating]} /> : null}
-          <TouchableHighlight style={this.state.business.name.length > 20 ? localStyles.smallButton : localStyles.smallButton} underlayColor={'#68a0ff'} onPress={this._handleOnClick}>
+          {this.state.business.foodRating ? (
+            <Image
+              style={{ width: 115, height: 58, margin: 5 }}
+              source={hygiene_rating[this.state.business.foodRating]}
+            />
+          ) : null}
+          <TouchableHighlight
+            style={localStyles.smallButton}
+            underlayColor={'#68a0ff'}
+            onPress={this._handleOnClick}
+          >
             <Text style={localStyles.buttonText}>Close</Text>
           </TouchableHighlight>
         </View>
-      )
-    } else return <View style={localStyles.inner}>
-      <Text style={localStyles.loading}>Loading...</Text>
-    </View>
+      );
+    } else
+      return (
+        <View style={localStyles.inner}>
+          <Text style={localStyles.loading}>Loading...</Text>
+        </View>
+      );
   }
 
   componentDidMount() {
@@ -63,43 +123,50 @@ export default class BusinessModal extends Component {
   }
 
   _fetchData = () => {
-    let businessName = this.props.business.name.toLowerCase().replace(/\s/g, '+').replace(/&/g, 'and');
-    fetch(`https://0p83k3udwg.execute-api.us-east-1.amazonaws.com/dev/api/device/business/${this.props.business.id}/${businessName}`)
+    let businessName = this.props.business.name
+      .toLowerCase()
+      .replace(/\s/g, '+')
+      .replace(/&/g, 'and');
+    fetch(
+      `https://0p83k3udwg.execute-api.us-east-1.amazonaws.com/dev/api/device/business/${
+        this.props.business.id
+      }/${businessName}`
+    )
       .then(buffer => buffer.json())
       .then(res => {
         if (res.message) {
-          this.setState({ error: true }, () => this._redirectToApp())
-        } else if (res.hours !== null) this.setState({ business: res })
-        else this.setState({
-          business: {
-            ...res,
-            hours: Array.from({ length: 7 }, () => 'No Listing')
-          }
-        })
+          this.setState({ error: true }, () => this._redirectToApp());
+        } else if (res.hours !== null) this.setState({ business: res });
+        else
+          this.setState({
+            business: {
+              ...res,
+              hours: Array.from({ length: 7 }, () => 'No Listing')
+            }
+          });
       });
-  }
+  };
 
   _handleOnClick = () => {
     this.props.closeModal();
-  }
+  };
 
   _redirectToApp = () => {
-    this.props.displayError()
-  }
+    this.props.displayError();
+  };
 
   _yelpLink = () => {
-    Linking.openURL(this.state.business.url)
-    .catch(() =>
+    Linking.openURL(this.state.business.url).catch(() =>
       this.props.displayError()
     );
-  }
+  };
 }
 
 BusinessModal.propTypes = {
   business: propTypes.object,
   closeModal: propTypes.func,
   displayError: propTypes.func
-}
+};
 
 const localStyles = StyleSheet.create({
   inner: {
@@ -116,22 +183,17 @@ const localStyles = StyleSheet.create({
     fontWeight: 'bold',
     paddingTop: 20
   },
+  businessInfo: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'white'
+  },
   buttonText: {
     fontFamily: 'Avenir',
     color: '#fff',
     textAlign: 'center',
     fontSize: 23,
     textAlignVertical: 'center'
-  },
-  button: {
-    height: 40,
-    width: 100,
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginTop: 2,
-    marginBottom: 10,
-    backgroundColor: '#1D976C',
-    borderRadius: 10
   },
   smallButton: {
     height: 40,
@@ -151,7 +213,7 @@ const localStyles = StyleSheet.create({
     marginTop: 5,
     marginBottom: 5,
     backgroundColor: '#39ac73',
-    borderRadius: 10,
+    borderRadius: 10
   },
   images: {
     flexDirection: 'row',
